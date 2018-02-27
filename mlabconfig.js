e1a11a48056730975c30d37f7c4a6a6728d1dbac
Mark Ewing <mewing@eastman.com>
2018-02-27 17:34:32 +0000

    Removing login details from GitHub (for real)

--- mlabconfig.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var mlabconfig = { admin: 'mongodb://admin:Ino3LyjSTeqCbqrfVo1oyc@ds231758.mlab.com:31758/tricitieseggdrop' };
-exports.default = mlabconfig;
